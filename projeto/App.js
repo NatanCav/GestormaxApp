dbf8f75d49b6f9792d653d0f5b32a81d36f86323
Natan Cavalcante <wylucasyw@gmail.com>
2025-05-21 21:33:35 -0300

    Mudança de imagem

--- projeto/App.js
+++ projeto/App.js
@@ -1,15 +1,39 @@
 // Importações dos componentes
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
-const img = require("./assets/gato.jpg")
+const img = require("./assets/macaco.jpg")
 // Função padrão
 export default function HomeScreen() {
   return (
     // Denominando View como "container" para estilização
     // Denominando Text como "title" para estilização
     <View style={styles.container}>
-      <Text style={styles.titulo}>Hello World!</Text>
-      <Image source={img} />
+      
+      <Text style={styles.titulo}>Bem Vindo Macaco</Text>
+      
+      <Image style={styles.img1} source={img}  />
+      
+      <TextInput 
+      style={styles.input} 
+      placeholder='Nome' 
+      autoCorrect={false}
+      />
+
+      <TextInput 
+      style={styles.input} 
+      placeholder='Email' 
+      autoCapitalize='none' 
+      keyboardType='email-address' 
+      autoCorrect={false}
+      />
+
+      <TextInput 
+      style={styles.input} 
+      placeholder='Senha' 
+      autoCapitalize='none' 
+      secureTextEntry 
+      autoCorrect={false}
+      />
     </View>
   );
 }
@@ -18,12 +42,29 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   titulo: {
     fontSize: 20,
     fontWeight: "bold",
+  },
+  img1: {
+    width: 150,
+    height: 200,
+    marginVertical: 10,
+    
+  },
+  input: {
+    borderWidth: 1,
+    width: 300,
+    height: 45,
+    borderRadius: 6,
+    borderColor: "black",
+    paddingHorizontal: 10,
+    fontSize: 16,
+    fontWeight: 600,
+    backgroundColor: '#e2e9f0',
+    marginVertical: 10,
   }
 });
