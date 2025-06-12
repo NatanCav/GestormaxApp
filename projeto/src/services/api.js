ea5103ec28f5be4801263c9512b6e22f9bd92630
Natan Cavalcante <wylucasyw@gmail.com>
2025-06-12 11:05:17 -0300

    Ip emulador

--- projeto/src/services/api.js
+++ projeto/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // ao usar o emulador, deverá colocar o IP do mesmo no lugar do 'localhost'
-  baseURL: 'http://192.168.31.134:8080//'
+  baseURL: 'http://192.168.10.13:8081'
 });
 
 export default api;
